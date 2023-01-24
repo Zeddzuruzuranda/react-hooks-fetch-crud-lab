1720ee61458965d22b377d755b85332f0c93355b
Zeddzuruzuranda <raphomelvo@gmail.com>
2023-01-24 15:31:57 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,14 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [currentPage, setCurrentPage] = useState("List");
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
       {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      <AdminNavBar PageChange={setCurrentPage} />
+      {currentPage === "Form" ? <QuestionForm /> : <QuestionList />}
     </main>
   );
 }
